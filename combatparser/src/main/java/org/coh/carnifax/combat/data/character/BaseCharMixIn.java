d5d5a935e3cbc29996322b5ddd248d59dd4c3d74
Carnifax <jonathan.downey@paragon-europe.com>
2021-04-06 18:20:20 +0100

    Better handling of session starts, when we don't know who someone is (like at Midnight)

---
+++ combatparser/src/main/java/org/coh/carnifax/combat/data/character/BaseCharMixIn.java
@@ -0,0 +1,11 @@
+package org.coh.carnifax.combat.data.character;
+
+import org.coh.carnifax.combat.data.character.summary.PowerSummary;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public abstract class BaseCharMixIn {
+	@JsonIgnore
+	private PowerSummary dps;
+
+}
